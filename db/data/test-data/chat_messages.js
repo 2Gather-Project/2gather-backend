8ff1282b99031c56ef0996181540359227641fb7
George Constantin <george.constantin33@gmail.com>
2025-04-14 10:56:49 +0100

    populated test tables

---
+++ db/data/test-data/chat_messages.js
@@ -0,0 +1,38 @@
+module.exports = [
+  {
+    chat_id: 1,
+    message: "Hi. Nice to see you",
+    created_at: 1604728980000,
+    user_id: 1,
+  },
+  {
+    chat_id: 1,
+    message: "Hey, nice app",
+    created_at: 1604728980002,
+    user_id: 2,
+  },
+  {
+    chat_id: 2,
+    message: "Hi. Nice to see you",
+    created_at: 1604728980003,
+    user_id: 1,
+  },
+  {
+    chat_id: 2,
+    message: "Hi there. Where should we meet?",
+    created_at: 1604728980004,
+    user_id: 3,
+  },
+  {
+    chat_id: 3,
+    message: "This is my first event",
+    created_at: 1604728980008,
+    user_id: 2,
+  },
+  {
+    chat_id: 3,
+    message: "Mine too. It will be exciting",
+    created_at: 1604728980010,
+    user_id: 3,
+  },
+];
