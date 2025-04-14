eb0b60f15e45ed6e4818866db80dbf5e30979cb9
George Constantin <george.constantin33@gmail.com>
2025-04-14 10:14:24 +0100

    seed structure

---
+++ db/data/development-data/index.js
@@ -0,0 +1,10 @@
+exports.usersData = require("./users");
+exports.blockedUsersData = require("./blocked_users");
+exports.chatMessagesData = require("./chat_messages");
+exports.chatRoomsData = require("./chat_rooms");
+exports.eventStatusData = require("./event_status");
+exports.eventUserActivityData = require("./event_user_activity");
+exports.eventsData = require("./events");
+exports.friendRequestsData = require("./friend_requests")
+exports.interestsData = require("./interests");
+
---
+++ setup-dbs.sql
@@ -0,0 +1,5 @@
+DROP DATABASE IF EXISTS gather;
+CREATE DATABASE gather;
+
+DROP DATABASE IF EXISTS gather_test;
+CREATE DATABASE gather_test;
