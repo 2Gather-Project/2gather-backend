e483d159b0bcbf41ba86f5b5b0b89596dc86a90a
shrutikhetan0801 <shrutikhetan0801@gmail.com>
2025-04-14 11:09:24 +0100

    [Committing for data]

---
+++ db/data/development-data/interests.js
@@ -0,0 +1,30 @@
+module.exports = [
+  {
+    id: 1,
+    name: "Cooking",
+  },
+  {
+    id: 1,
+    name: "Cooking",
+  },
+  {
+    id: 2,
+    name: "Dancing",
+  },
+  {
+    id: 3,
+    name: "Dog Walking",
+  },
+  {
+    id: 4,
+    name: "Theater",
+  },
+  {
+    id: 5,
+    name: "Movies",
+  },
+  {
+    id: 6,
+    name: "Reading",
+  },
+];
