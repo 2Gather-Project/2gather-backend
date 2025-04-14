5b71a9aa653dbbf34bcb2b04ab4a42df0f38e38c
George Constantin <george.constantin33@gmail.com>
2025-04-14 15:27:30 +0100

    db in prog

--- db/data/test-data/interests.js
+++ db/data/test-data/interests.js
@@ -1,24 +1,5 @@
 module.exports = [
-    {
-      name: "Cooking",
-    },
-    {
-      name: "Cooking",
-    },
-    {
-      name: "Dancing",
-    },
-    {
-      name: "Dog Walking",
-    },
-    {
-      name: "Theater",
-    },
-    {
-      name: "Movies",
-    },
-    {
-      name: "Reading",
-    },
-  ];
-  
+  {
+    enum: ["Cooking", "Dancing", "Dog Walking", "Theater", "Reading"],
+  },
+];
--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -11,10 +11,25 @@ const seed = ({
   friendRequestsData,
   interestData,
 }) => {
-  return db.query("DROP TABLE IF EXISTS users").then(() => {
-    return createUsers();
-  });
+  return db
+    .query("DROP TABLE IF EXISTS users")
+    .then(() => {
+      db.query("DROP TABLE IF EXISTS interests");
+    })
+    .then(() => {
+      return createInterests();
+    })
+    .then(() => {
+      return createUsers();
+    });
 };
+
+function createInterests() {
+  return db.query(
+    `CREATE TYPE interests AS
+      ENUM('Cooking', 'Dancing', 'Dog Walking', 'Theater', 'Reading')`,
+  );
+}
 
 function createUsers() {
   return db.query(
@@ -34,6 +49,23 @@ function createUsers() {
     job_title VARCHAR,
     coffee_tea VARCHAR,
     image_url VARCHAR)`,
+  );
+}
+
+function createEvents() {
+  return db.query(
+    `CREATE TABLE events (
+    event_id SERIAL PRIMARY KEY,
+    user_id VARCHAR NOT NULL,
+    title VARCHAR NOT NULL,
+    description VARCHAR NOT NULL,
+    location VARCHAR NOT NULL,
+    category VARCHAR,
+    time VARCHAR,
+    created_at DATE,
+    FOREIGN KEY (category) REFERENCES interest(name) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+   `,
   );
 }
 
