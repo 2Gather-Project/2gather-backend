116158d6d973e9aebbded43260afdf318c4ac727
Katie Hardy <katiehardy@MacBookPro-2.broadband>
2025-04-15 14:23:35 +0100

    supabase setup

--- db/connection.js
+++ db/connection.js
@@ -12,9 +12,9 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
 
 const config = {};
 
-// if (ENV === "production") {
-//   config.connectionString = process.env.DATABASE_URL;
-//   config.max = 2;
-// }
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
 console.log(ENV);
 module.exports = new Pool(config);
