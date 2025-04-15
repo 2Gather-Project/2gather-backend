69de2a34dec65b8c8d29ce900e92f269fc7f6d54
shrutikhetan0801 <shrutikhetan0801@gmail.com>
2025-04-15 16:35:18 +0100

    Committing endpoint in progress

--- app.js
+++ app.js
@@ -2,12 +2,18 @@
 // import {getEndpoints} from "./controller/endpoints.controller.js";
 const { getEndpoints } = require("./controller/endpoints.controller.js");
 const fastify = require("fastify");
+const { allUsers } = require("./controller/users.controller.js");
+const { getEvents } = require("./controller/events.controller.js");
 
 const fastifyApp = fastify({
   logger: true,
 });
 
 fastifyApp.get("/api", getEndpoints);
+
+fastifyApp.get("/api/users", allUsers);
+
+fastifyApp.get("/api/events", getEvents);
 
 fastifyApp.listen({ port: 3000 }, (err, address) => {
   if (err) {
