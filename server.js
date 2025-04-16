0b19ea3b6c0153f4be5a0f6af871e53204aa56c7
George Constantin <george.constantin33@gmail.com>
2025-04-16 18:04:47 +0100

    render host in prog

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const fastifyApp = require("./app");
 
-fastifyApp.listen({ port: 3000 }, (err, address) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
-  console.log(`Server listening at ${address}`);
-});
+fastifyApp.listen(
+  { port: process.env.PORT || 3000, host: "0.0.0.0" },
+  (err, address) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    console.log(`Server listening at ${address}`);
+  },
+);
