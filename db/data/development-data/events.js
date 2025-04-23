c5c788bc7c71c9efefcd1f2f8688cfcb5e75f61b
George Constantin <george.constantin33@gmail.com>
2025-04-23 12:14:52 +0100

    added data to events and image_urls

--- db/data/development-data/events.js
+++ db/data/development-data/events.js
@@ -6,6 +6,8 @@ module.exports = [
     location: "London",
     time: "13:00:00",
     created_at: "2025-04-14",
+    image_url:
+      "https://images.unsplash.com/photo-1696694139314-e0e5962b8dc0?q=80&w=1970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     user_id: 2,
@@ -14,6 +16,8 @@ module.exports = [
     location: "Nottingham",
     time: "14:00:00",
     created_at: "2025-04-26",
+    image_url:
+      "https://images.unsplash.com/photo-1502157152-32f452ffec7d?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     user_id: 3,
@@ -22,6 +26,8 @@ module.exports = [
     location: "Lincoln",
     time: "15:00:00",
     created_at: "2025-04-26",
+    image_url:
+      "https://images.unsplash.com/photo-1610890716073-bda3d1149963?q=80&w=1931&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     user_id: 4,
@@ -31,6 +37,8 @@ module.exports = [
     location: "London",
     time: "13:00:00",
     created_at: "2025-04-20",
+    image_url:
+      "https://images.unsplash.com/photo-1653387137517-fbc54d488ed8?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     user_id: 3,
@@ -40,6 +48,8 @@ module.exports = [
     location: "Edinburgh",
     time: "14:00:00",
     created_at: "2025-04-20",
+    image_url:
+      "https://images.unsplash.com/photo-1569154076682-4c0466623ec2?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     user_id: 4,
@@ -49,6 +59,8 @@ module.exports = [
     location: "Manchester",
     time: "14:15:00",
     created_at: "2025-04-26",
+    image_url:
+      "https://images.unsplash.com/photo-1545324053-41b04f1a8e8a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     user_id: 4,
@@ -58,5 +70,118 @@ module.exports = [
     location: "Bristol",
     time: "11:00:00",
     created_at: "2025-04-14",
+    image_url:
+      "https://images.unsplash.com/photo-1744748208244-79bc6420d29f?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    user_id: 1,
+    title: "Sunset Picnic at Primrose Hill",
+    description: "Join me for a relaxing picnic and great views of the city.",
+    location: "London",
+    time: "18:30:00",
+    created_at: "2025-04-20",
+    image_url:
+      "https://images.unsplash.com/photo-1503803548695-c2a7b4a5b875?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    user_id: 1,
+    title: "Board Games Night at My Flat",
+    description: "Bring your favorite board game and let's hang out!",
+    location: "London",
+    time: "19:00:00",
+    created_at: "2025-04-19",
+    image_url:
+      "https://images.unsplash.com/photo-1656686631067-401ccf8d17a1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    user_id: 1,
+    title: "Coffee & Code at a Local Café",
+    description: "Chill coding session for devs and coffee lovers.",
+    location: "London",
+    time: "10:00:00",
+    created_at: "2025-04-21",
+    image_url:
+      "https://images.unsplash.com/photo-1669023414180-4dcf35d943e1?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    user_id: 1,
+    title: "Sunday Hike in Hampstead Heath",
+    description: "Stretch your legs and clear your mind on a scenic walk.",
+    location: "London",
+    time: "11:00:00",
+    created_at: "2025-04-22",
+    image_url:
+      "https://images.unsplash.com/photo-1722529912963-0841d33da33b?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    user_id: 1,
+    title: "Live Jazz & Drinks at Ronnie Scott’s",
+    description: "Catch some smooth jazz and enjoy a laid-back evening.",
+    location: "London",
+    time: "20:00:00",
+    created_at: "2025-04-23",
+    image_url:
+      "https://images.unsplash.com/photo-1548163111-bc419d75fef4?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    user_id: 2,
+    title: "Street Food Tour in Camden",
+    description:
+      "Taste your way through the best street food stalls in Camden.",
+    location: "London",
+    time: "14:00:00",
+    created_at: "2025-04-20",
+    image_url:
+      "https://images.unsplash.com/photo-1672002586825-abcf586b07cb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y2FtZGVufGVufDB8fDB8fHww",
+  },
+  {
+    user_id: 2,
+    title: "Book Club at the Local Library",
+    description: "Let’s discuss this month’s book and share our thoughts.",
+    location: "London",
+    time: "17:30:00",
+    created_at: "2025-04-21",
+    image_url:
+      "https://plus.unsplash.com/premium_photo-1706061121084-1616b4356e16?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    user_id: 2,
+    title: "Photography Walk by the Thames",
+    description: "Explore the riverfront and snap some great photos together.",
+    location: "London",
+    time: "15:00:00",
+    created_at: "2025-04-22",
+    image_url:
+      "https://images.unsplash.com/photo-1549622391-dfb881b07670?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    user_id: 3,
+    title: "Yoga in the Park",
+    description: "Relax, stretch, and find your zen with some group yoga.",
+    location: "London",
+    time: "09:00:00",
+    created_at: "2025-04-20",
+    image_url:
+      "https://images.unsplash.com/photo-1696885385698-d99c90cda091?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHlvZ2F8ZW58MHx8MHx8fDA%3D",
+  },
+  {
+    user_id: 3,
+    title: "Movie Night: Indie Film Screening",
+    description: "Watch and discuss a unique indie film in a cozy venue.",
+    location: "London",
+    time: "20:00:00",
+    created_at: "2025-04-21",
+    image_url:
+      "https://images.unsplash.com/photo-1520088258008-0f0a636a00a9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bW92aWV8ZW58MHx8MHx8fDA%3D",
+  },
+  {
+    user_id: 3,
+    title: "Late Brunch & Chat at the Diner",
+    description: "Catch up over pancakes and good vibes.",
+    location: "London",
+    time: "11:30:00",
+    created_at: "2025-04-23",
+    image_url:
+      "https://plus.unsplash.com/premium_photo-1692309186545-24e114562a5c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YnJ1bmNofGVufDB8fDB8fHww",
   },
 ];
